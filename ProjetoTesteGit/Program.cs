60d4a4bbf6df09bd7c001b3e904ca150014c021d
Lucas Souza <desouza655@gmail.com>
2020-09-15 14:01:47 -0300

    acrescentado pergunta dnv

--- ProjetoTesteGit/Program.cs
+++ ProjetoTesteGit/Program.cs
@@ -5,6 +5,7 @@ namespace ProjetoTesteGit {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia!");
+            Console.WriteLine("Como tut ta?");
         }
     }
 }
